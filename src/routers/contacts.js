28e1a48b78e00156fae2b578901463670ec5caef
Vedat <vedatbasaq@gmail.com>
2025-11-26 15:05:39 +0300

    hw3crud

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -2,11 +2,18 @@ import { Router } from 'express';
 import {
   getContactsController,
   getContactByIdController,
+  createContactController,
+  updateContactController,
+  deleteContactController,
 } from '../controllers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const contactsRouter = Router();
 
-contactsRouter.get('/', getContactsController);
-contactsRouter.get('/:contactId', getContactByIdController);
+contactsRouter.get('/', ctrlWrapper(getContactsController));
+contactsRouter.get('/:contactId', ctrlWrapper(getContactByIdController));
+contactsRouter.post('/', ctrlWrapper(createContactController));
+contactsRouter.patch('/:contactId', ctrlWrapper(updateContactController));
+contactsRouter.delete('/:contactId', ctrlWrapper(deleteContactController));
 
 export default contactsRouter;
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -8,3 +8,21 @@ export const getContactById = async (contactId) => {
   const contact = await Contact.findById(contactId);
   return contact;
 };
+
+export const createContact = async (payload) => {
+  const contact = await Contact.create(payload);
+  return contact;
+};
+
+export const updateContact = async (contactId, payload) => {
+  const updated = await Contact.findByIdAndUpdate(contactId, payload, {
+    new: true,
+    runValidators: true,
+  });
+  return updated;
+};
+
+export const deleteContact = async (contactId) => {
+  const deleted = await Contact.findByIdAndDelete(contactId);
+  return deleted;
+};
