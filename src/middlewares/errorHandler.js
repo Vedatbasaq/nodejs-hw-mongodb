2b8f49e06d7cb3835f9ded814527c31b15f49c49
Vedat <vedatbasaq@gmail.com>
2025-12-16 16:58:09 +0300

    .env and errrorhandler.js update

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,5 +1,5 @@
 export const errorHandler = (err, req, res, next) => {
-  const status = 500;
+  const status = err.status || 500;
   const message = err?.message || 'Internal Server Error';
   const payload = { status, message };
   if (process.env.NODE_ENV === 'development' && err?.stack) {
